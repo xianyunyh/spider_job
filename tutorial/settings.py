c4f306d3c693d564f2ce10b95238a2fe0ef06f6f
=xianyunyehe <tianlei542475736@gmail.com>
2018-07-29 20:42:25 +0800

    '修正接口地址'

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -5,7 +5,7 @@ BOT_NAME = 'tutorial'
 SPIDER_MODULES = ['tutorial.spiders']
 NEWSPIDER_MODULE = 'tutorial.spiders'
 
-
+LOG_LEVEL = 'ERROR'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
 
