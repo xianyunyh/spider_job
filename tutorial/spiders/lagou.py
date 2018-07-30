b59fb6ad8954f36d68cdf29677a680648887fc05
=xianyunyehe <tianlei542475736@gmail.com>
2018-07-30 23:48:50 +0800

    '修正脚本'

--- tutorial/spiders/lagou.py
+++ tutorial/spiders/lagou.py
@@ -15,14 +15,14 @@ class ZhipinSpider(scrapy.Spider):
     current_page = 1
     # 上海PHP
     start_urls = [
-        "https://m.lagou.com/search.json?city=%E4%B8%8A%E6%B5%B7&positionName=PHP&pageSize=50&pageNo=1",
+        "https://m.lagou.com/search.json?city=%E4%B8%8A%E6%B5%B7&positionName=PHP&pageSize=10",
     ]
     custom_settings = {
         "ITEM_PIPELINES":{
             'tutorial.pipelines.TutorialPipeline': 300,
         },
         "DOWNLOADER_MIDDLEWARES":{
-            'tutorial.middlewares.LagouMiddleware': 299,
+            #'tutorial.middlewares.LagouMiddleware': 299,
         },
         "DEFAULT_REQUEST_HEADERS":{
             'Accept': 'application/json',
@@ -30,7 +30,7 @@ class ZhipinSpider(scrapy.Spider):
             'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
             'Referer':' https://m.lagou.com/search.html',
             'X-Requested-With':"XMLHttpRequest",
-            "cookie":"user_trace_token=20180725211521-aa09c44d3ab54bf2bde9654cc6c4ab35; _ga=GA1.3.881896906.1532524522; _ga=GA1.2.881896906.1532524522; _gid=GA1.2.726017449.1532524522; LGUID=20180725211522-c92c8f9d-900c-11e8-a44e-525400f775ce; index_location_city=%E4%B8%8A%E6%B5%B7; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1532362837,1532398323,1532418933,1532572655; JSESSIONID=ABAAABAAAFDABFGC547325A2971F2AA49C9A368C145B679; _gat=1; LGSID=20180726124253-5bcf406b-908e-11e8-9ee6-5254005c3644; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fm.lagou.com%2Fjobs%2F4741044.html; PRE_LAND=https%3A%2F%2Fm.lagou.com%2Fsearch.html; X_HTTP_TOKEN=1f8ff879c39e0209e7efb23aad68da74; LG_LOGIN_USER_ID=52613e3ab983a93b76c62f27b7f9f49934626b1999bbab20; _putrc=51AF6FA824D5BE21; login=true; unick=%E7%94%B0%E9%9B%B7; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1532580490; LGRID=20180726124809-182de556-908f-11e8-a494-525400f775ce"
+            "cookie":"user_trace_token=20180729204908-c52cbd0e4d394a3cb58519920e083a0b; JSESSIONID=ABAAABAAAGCABCC79240AC570FA1732FC9159DD43105445; X_HTTP_TOKEN=5338df7c911167920d5f0d7615b94748; _ga=GA1.2.1539481412.1532868549; _gid=GA1.2.309176990.1532868549; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1532398323,1532418933,1532572655,1532868549; LGSID=20180729204908-c8b50abb-932d-11e8-ab47-525400f775ce; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fpassport.lagou.com%2Flogin%2Flogin.html%3Fts%3D1532868548009%26serviceId%3Dlagou%26service%3Dhttp%25253A%25252F%25252Fm.lagou.com%25252Fuser%25252Fcollect.json%25253FpositionId%25253D233753%26action%3Dlogin%26signature%3D01759552449CC48F903E9AB889298EAC; LGUID=20180729204908-c8b50c64-932d-11e8-ab47-525400f775ce; _ga=GA1.3.1539481412.1532868549; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1532868713; LGRID=20180729205153-2ae970a6-932e-11e8-a07d-5254005c3644"
         }
     }
     def parse(self, response):
@@ -41,8 +41,14 @@ class ZhipinSpider(scrapy.Spider):
         for item in items:
             url = 'https://m.lagou.com/jobs/'
             url += str(item['positionId'])+".html"
-            yield Request(url,callback=self.parse_item,meta=item)
-        time.sleep(int(random.uniform(60,80)))
+            print(url)
+            time.sleep(int(random.uniform(160,180)))
+            yield Request(url,callback=self.parse_item,meta=item,headers=pc_header)
+        if self.current_page < 4:
+            self.current_page += 1
+            list_url = self.start_urls[0]+"&pageNo="+str(self.current_page)
+            time.sleep(int(random.uniform(160,180)))
+            yield Request(list_url,callback=self.parse)
     def parse_item(self,response):
         item = TutorialItem()
         q = response.css
@@ -53,9 +59,9 @@ class ZhipinSpider(scrapy.Spider):
             item['address'] = match_obj.group(1)
         item['salary'] = response.meta['salary']
         item['create_time'] = response.meta['createTime']
-        item['body'] = q('.positiondesc .content').xpath("string(.)").extract_first()
+        item['body'] = q('.content').xpath('string(.)').extract_first()
         item['company_name']  = response.meta['companyName']
-        item['postion_id'] = response.url.split("/")[-1].split('.')[0]
+        item['postion_id'] = response.meta['positionId']
         item['position_name'] = response.meta['positionName']
-        time.sleep(int(random.uniform(60,80)))
+    #print(item)
         yield  item
