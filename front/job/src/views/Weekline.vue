7e3305111b25f8c4097c13d3d84bbfbacb31111f
=xianyunyehe <tianlei542475736@gmail.com>
2018-08-06 22:32:18 +0800

    '修正'

--- front/job/src/views/Weekline.vue
+++ front/job/src/views/Weekline.vue
@@ -1,38 +1,35 @@
 <template>
 <div class="container">
-<ve-line :data="chartData" :settings="chartSettings" :colors="chartSettings.colors"></ve-line>
+<ve-line :data="chartData" :settings="chartSettings"></ve-line>
 </div>
 </template>
 
 
 <script>
-import {get_week_line} from '@/service/api'
-import VeLine from 'v-charts/lib/line.common'
+import { get_week_line } from "@/service/api";
+import VeLine from "v-charts/lib/line.common";
 export default {
   components: { VeLine },
-  data () {
+  data() {
     return {
-        chartSettings:{
-            labelMap: {
-            'date': '日期',
-            'count': '发布数'
-            },
-            yAxisType:["normal"],
-            colors:['#c23531','#2f4554', '#61a0a8',
-                    '#d48265', '#91c7ae','#749f83', 
-                    '#ca8622', '#bda29a','#6e7074',
-                    '#546570', '#c4ccd3']
-        },
-        chartData: {
-            columns: ['date', 'count'],
-            rows: [
-            ]
+      chartSettings: {
+        labelMap: {
+          date: "发布时间",
+          count: "发布次数"
         }
+      },
+      chartData: {}
+    };
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      let rows = await get_week_line();
+      let columns = Object.keys(this.chartSettings["labelMap"]);
+      this.chartData = { columns, rows };
     }
-  },
-  async created() {
-        let data =  await get_week_line()
-        this.chartData.rows = data
-  },
-}
+  }
+};
 </script>
